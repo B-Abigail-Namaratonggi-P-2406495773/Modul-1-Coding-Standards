0f13f3b3d9120ae73f174261d1dcc600a376bfc2
namarabigail <bigelnp10@gmail.com>
2026-03-10 15:32:03 +0700

    [RED] Add functional tests for order history and search

--- src/test/java/id/ac/ui/cs/advprog/eshop/controller/OrderControllerTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/controller/OrderControllerTest.java
@@ -9,6 +9,7 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(OrderController.class)
@@ -28,5 +29,21 @@ public class OrderControllerTest {
         mockMvc.perform(get("/order/create"))
                 .andExpect(status().isOk())
                 .andExpect(view().name("createOrder"));
+    }
+
+    @Test
+    public void testOrderHistoryPage() throws Exception {
+        mockMvc.perform(get("/order/history"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("historyOrder"));
+    }
+
+    @Test
+    public void testOrderHistoryPost() throws Exception {
+        mockMvc.perform(post("/order/history")
+                        .param("author", "Abigail"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("listOrder"))
+                .andExpect(model().attributeExists("orders"));
     }
 }
