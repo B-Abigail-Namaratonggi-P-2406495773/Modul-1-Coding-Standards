aede191549b5bd7f7e1c96681bfd42c0ac066bbe
namarabigail <bigelnp10@gmail.com>
2026-03-10 15:34:42 +0700

    [GREEN] Implement order history in OrderController

--- src/main/java/id/ac/ui/cs/advprog/eshop/controller/OrderController.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/controller/OrderController.java
@@ -1,5 +1,6 @@
 package id.ac.ui.cs.advprog.eshop.controller;
 
+import id.ac.ui.cs.advprog.eshop.model.Order;
 import id.ac.ui.cs.advprog.eshop.model.Product;
 import id.ac.ui.cs.advprog.eshop.service.OrderService;
 import id.ac.ui.cs.advprog.eshop.service.ProductService;
@@ -25,5 +26,17 @@ public class OrderController {
         List<Product> products = productService.findAll();
         model.addAttribute("products", products);
         return "createOrder";
+    }
+
+    @GetMapping("/history")
+    public String orderHistoryPage() {
+        return "historyOrder";
+    }
+
+    @PostMapping("/history")
+    public String orderHistoryPost(@RequestParam String author, Model model) {
+        List<Order> orders = orderService.findAllByAuthor(author);
+        model.addAttribute("orders", orders);
+        return "listOrder";
     }
 }
